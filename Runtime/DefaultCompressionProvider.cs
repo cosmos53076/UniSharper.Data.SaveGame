d4b23c56ed4e9b9a913110ecf7d441686590dcbf
Jerry Lee <cosmos53076@163.com>
2024-10-16 14:27:46 +0800

    Updates the version of UniSharper.Core dependency.

--- Runtime/DefaultCompressionProvider.cs
+++ Runtime/DefaultCompressionProvider.cs
@@ -1,8 +1,7 @@
 // Copyright (c) Jerry Lee. All rights reserved. Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
-using System.IO;
-using System.IO.Compression;
+using ReSharp.Compression;
 
 namespace UniSharper.Data.SaveGame
 {
@@ -11,33 +10,8 @@ namespace UniSharper.Data.SaveGame
     /// </summary>
     public class DefaultCompressionProvider : ICompressionProvider
     {
-        public byte[] Compress(byte[] input)
-        {
-            using (var outputStream = new MemoryStream())
-            {
-                using (var deflateStream = new DeflateStream(outputStream, CompressionLevel.Fastest))
-                {
-                    deflateStream.Write(input, 0, input.Length);
-                    deflateStream.Flush();
-                }
+        public byte[] Compress(byte[] input) => Deflate.Compress(input);
 
-                return outputStream.ToArray();
-            }
-        }
-
-        public byte[] Decompress(byte[] input)
-        {
-            using (var inputStream = new MemoryStream(input))
-            {
-                using (var deflateStream = new DeflateStream(inputStream, CompressionMode.Decompress))
-                {
-                    using (var outputStream = new MemoryStream())
-                    {
-                        deflateStream.CopyTo(outputStream);
-                        return outputStream.ToArray();
-                    }
-                }
-            }
-        }
+        public byte[] Decompress(byte[] input) => Deflate.Decompress(input);
     }
 }
